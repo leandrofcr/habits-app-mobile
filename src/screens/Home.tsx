d10b07a4752dec089b40cf6738e4b246d2c4cbd4
Leandro Reis <lleandrofr@gmail.com>
2023-01-23 12:46:40 -0300

    feat: add placeholder days in the HabitDay days

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { generateRangeDatesFromYearStart } from "../utils/generate-dates-from-year-beginning";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimumSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -21,7 +25,25 @@ export function Home() {
           </Text>
         ))}
       </View>
-      <HabitDay />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
